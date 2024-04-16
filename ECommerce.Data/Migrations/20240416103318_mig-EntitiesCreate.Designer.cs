855f260edf17ac67218efa9754cff7ccdb47e587
305-öğrenci <305-renci@DESKTOP-PO3KMEU>
2024-04-16 13:36:58 +0300

    Category, Product, Order and OrderLine entities have been created

--- ECommerce.Data/Migrations/20240416103318_mig-EntitiesCreate.Designer.cs
+++ ECommerce.Data/Migrations/20240416103318_mig-EntitiesCreate.Designer.cs
@@ -4,6 +4,7 @@ using ECommerce.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerce.Data.Migrations
 {
     [DbContext(typeof(ECommerceContext))]
-    partial class ECommerceContextModelSnapshot : ModelSnapshot
+    [Migration("20240416103318_mig-EntitiesCreate")]
+    partial class migEntitiesCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
